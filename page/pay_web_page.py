2ba1dbe5ab23739cdfe82632c1611563d170ad48
Li Yang <120624697@qq.com>
2021-09-02 13:59:18 +0800

    update element name

--- page/pay_web_page.py
+++ page/pay_web_page.py
@@ -9,7 +9,7 @@ class PayWebPage(Page):
         xpath="/html/body/div/div[2]/div/div/div/div[3]/div", describe="二级代理登")
     login_user_name_input = Element(css="#UserName", describe="登录用户名输入框")
     login_password_input = Element(css="#PassWord", describe="登录密码输入框")
-    login_user_code_input = Element(css="#UserCode", describe="登录验证码输入框")
+    login_verification_code_input = Element(css="#UserCode", describe="登录验证码输入框")
     login_button = Element(css="#LoginForm > button", describe="登录按钮")
     hh_buy_button = Element(
         xpath="//*[@id='page-wrapper']/div[3]/div[2]/div[5]/div/div/div[2]/div/a[1]", describe="通用系列产品购买按钮")
